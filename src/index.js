6f44e0e43058495cf2a21d7cc70872b8334c1e68
Hriday Kumar Pranit <hridaykp21@gmail.com>
2023-01-25 16:01:41 +0530

    middleware

--- src/index.js
+++ src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './componenets/App';
-import { legacy_createStore as createStore } from 'redux';
+import { legacy_createStore as createStore, applyMiddleware } from 'redux';
 import  rootReducer  from './reducers';
 
+//function logger(obj, next, action) => CURRIED Function
+const logger = function({dispatch, getState}){
+  return function(next){
+    return function(action){
+      //middleware code
+      console.log('ACTION_TYPE = ', action.type);
+      next(action);
+    }
+  }
+}
 
-const store = createStore(rootReducer);
+const store = createStore(rootReducer, applyMiddleware(logger));
 console.log('store =>', store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
