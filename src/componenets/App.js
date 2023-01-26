85c2e2a6217f6367e7f27d43f7867948984906d4
Hriday Kumar Pranit <hridaykp21@gmail.com>
2023-01-26 13:48:42 +0530

    used context api

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -3,7 +3,7 @@ import {data} from  '../data';
 import Navbar from './Navbar';
 import MovieCard from "./MovieCard";
 import { addMovies, setShowFavourites } from '../actions';
-// import { search } from "../reducers";
+import { StoreContext } from "..";
 class App extends React.Component {
     componentDidMount(){
         const {store} = this.props;
@@ -37,7 +37,7 @@ class App extends React.Component {
         const displayMovies = showFavourites ? favourites: list;
         return (
             <div className="App">
-                <Navbar dispatch={this.props.store.dispatch} search={search}/>
+                <Navbar search={search}/>
                 <div className="main">
                     <div className="tabs">
                         <div className={`tab ${showFavourites?'':'active-tabs'}`} onClick={()=>this.onChangeTab(false)}>
@@ -67,4 +67,15 @@ class App extends React.Component {
     
 }
 
-export default App;
+
+class AppWrapper extends React.Component{
+    render(){
+        return(
+            <StoreContext.Consumer>
+                {(store) => <App store={store}/>}
+            </StoreContext.Consumer>
+        )
+    }
+}
+
+export default AppWrapper;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import thunk from 'redux-thunk';
 import './index.css';
@@ -16,10 +16,25 @@ const logger = ({dispatch, getState}) => (next) => (action) => {
 const store = createStore(rootReducer, applyMiddleware(logger, thunk));
 console.log('store =>', store);
 
+export const StoreContext = createContext();
+console.log('StoreContext =>', StoreContext);
+
+class Provider extends React.Component{
+  render(){
+    const {store} = this.props;
+    return (
+      <StoreContext.Provider value={store}>
+        {this.props.children}
+      </StoreContext.Provider>
+    )
+  }
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App store={store}/>
+    <Provider store={store}>
+      <App/>
+    </Provider>
   </React.StrictMode>
 );
 
