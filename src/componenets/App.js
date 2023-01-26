5784fa7c4d77088c67c84a08394eefe7e147de68
Hriday Kumar Pranit <hridaykp21@gmail.com>
2023-01-26 15:05:25 +0530

    connecting componenet

--- src/componenets/App.js
+++ src/componenets/App.js
@@ -3,22 +3,15 @@ import {data} from  '../data';
 import Navbar from './Navbar';
 import MovieCard from "./MovieCard";
 import { addMovies, setShowFavourites } from '../actions';
-import { StoreContext } from "..";
+import { connect} from "..";
 class App extends React.Component {
     componentDidMount(){
-        const {store} = this.props;
-        store.subscribe(()=>{
-            console.log('UPDATED');
-            this.forceUpdate();
-        })
-        // make api call
-        //dispatch actions
-        store.dispatch(addMovies(data))
-        console.log('STATE', this.props.store.getState());
+        this.props.dispatch(addMovies(data))
+        // console.log('STATE', this.props.getState());
     }
 
     isMovieFavourite =(movie)=> {
-        const { movies }  = this.props.store.getState();
+        const { movies }  = this.props;
         const index = movies.favourites.indexOf(movie);
         if(index !== -1){
             return true; //found movie
@@ -28,12 +21,12 @@ class App extends React.Component {
         }
     }
     onChangeTab = (flag) =>{
-        this.props.store.dispatch(setShowFavourites(flag))
+        this.props.dispatch(setShowFavourites(flag))
     }
     render(){
-        const { movies, search } = this.props.store.getState(); 
+        const { movies, search } = this.props; 
         const {list, favourites, showFavourites} = movies;
-        console.log('RENDER', this.props.store.getState());
+        // console.log('RENDER', this.props.store.getState());
         const displayMovies = showFavourites ? favourites: list;
         return (
             <div className="App">
@@ -53,7 +46,7 @@ class App extends React.Component {
                                 <MovieCard 
                                     movie={movie} 
                                     key={`movies-${index}`} 
-                                    dispatch={this.props.store.dispatch}
+                                    dispatch={this.props.dispatch}
                                     isFavourite = {this.isMovieFavourite(movie)}
                                 />
                             )
@@ -68,14 +61,21 @@ class App extends React.Component {
 }
 
 
-class AppWrapper extends React.Component{
-    render(){
-        return(
-            <StoreContext.Consumer>
-                {(store) => <App store={store}/>}
-            </StoreContext.Consumer>
-        )
+// class AppWrapper extends React.Component{
+//     render(){
+//         return(
+//             <StoreContext.Consumer>
+//                 {(store) => <App store={store}/>}
+//             </StoreContext.Consumer>
+//         )
+//     }
+// }
+
+function mapStateToProps(state){
+    return{
+        movies: state.movies,
+        search: state.search
     }
 }
-
-export default AppWrapper;
+const connectedAppComponent = connect(mapStateToProps)(App);
+export default connectedAppComponent;
--- src/index.js
+++ src/index.js
@@ -29,6 +29,40 @@ class Provider extends React.Component{
     )
   }
 }
+//const connectedAppComponent = connect(callback)(App);
+export function connect(callback){
+  return function (Component) { 
+     class ConnectedComponent extends React.Component{
+      constructor(props){
+        super(props);
+        this.unsubscribe = this.props.store.subscribe(()=>this.forceUpdate())
+      }
+      componentWillUnmount(){
+        this.unsubscribe();
+      }
+      render(){
+        const {store} = this.props
+        const state = store.getState();
+        const dataToBePassedAsProx = callback(state)
+        return (
+          <Component {...dataToBePassedAsProx} 
+            dispatch = {store.dispatch}
+          />
+        )
+      }
+    }
+    class ConnectedComponentWrapper extends React.Component{
+      render(){
+        return(
+          <StoreContext.Consumer>
+            {store=> <ConnectedComponent store ={store}/>}
+          </StoreContext.Consumer>
+        )
+      }
+    }
+    return ConnectedComponentWrapper;
+  }
+}
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
